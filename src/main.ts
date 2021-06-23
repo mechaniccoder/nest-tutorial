1168e4d3e6673ab5379bf707dcd9847e3f6dbca5
mechaniccoder <yuseunghwan94@gmail.com>
2021-06-24 00:36:20 +0900

    Logger

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
   app.use(cookieParser());
   await app.listen(3000);
 }
